81c3cb3a5352c83b86db3afb1f27e1cc7acf70f7
Denis Neves Fróes Junior <denis.neves@smtsolucoes.com.br>
2020-05-10 15:50:33 -0300

    feat: Book model, migration and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  Book.create(
+    title: Faker::Book.title,
+    author: Faker::Book.author,
+    isbn: Faker::Code.isbn,
+    price: Faker::Number.number(digits: 10),
+  )
+end
